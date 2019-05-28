e22d3b442b6fa25498c29057e8e91b5a24e5d23c
travisbare <travisbare@gmail.com>
2019-05-28 15:01:18 -0500

    removed homework instructions

--- server.js
+++ server.js
@@ -43,50 +43,8 @@ db.once("open", function(){
     console.log("Connected to Mongoose!");
 })
 
-
-
 // port setup
 const PORT = process.env.PORT || 3000;
-
-// Routes
-
-// A GET route for scraping the echoJS website
-app.get("/scrape", function (req, res) {
-    // First, we grab the body of the html with axios
-    axios.get("http://www.nytimes.com/").then(function (response) {
-        // Then, we load that into cheerio and save it to $ for a shorthand selector
-        let $ = cheerio.load(response.data);
-
-        // Now, we grab every h2 within an article tag, and do the following:
-        $("article h2").each(function (i, element) {
-            // Save an empty result object
-            let result = {};
-
-            // Add the text and href of every link, and save them as properties of the result object
-            result.title = $(this)
-                .children("a")
-                .text();
-            result.link = $(this)
-                .children("a")
-                .attr("href");
-
-            // Create a new Article using the `result` object built from scraping
-            db.Article.create(result)
-                .then(function (dbArticle) {
-                    // View the added result in the console
-                    console.log(dbArticle);
-                })
-                .catch(function (err) {
-                    // If an error occurred, log it
-                    console.log(err);
-                });
-        });
-
-        // Send a message to the client
-        res.send("Scrape Complete");
-        console.log("Scrape Complete.");
-    });
-});
 
 // Route for getting all Articles from the db
 app.get("/articles", function (req, res) {
@@ -107,7 +65,7 @@ app.get("/articles/:id", function (req, res) {
     // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
     db.Article.findOne({ _id: req.params.id })
         // ..and populate all of the notes associated with it
-        .populate("note")
+        .populate("comment")
         .then(function (dbArticle) {
             // If we were able to successfully find an Article with the given id, send it back to the client
             res.json(dbArticle);
@@ -121,7 +79,7 @@ app.get("/articles/:id", function (req, res) {
 // Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function (req, res) {
     // Create a new note and pass the req.body to the entry
-    db.Note.create(req.body)
+    db.Comment.create(req.body)
         .then(function (dbNote) {
             // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
             // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
