eabeeaa16dab8d3bef8307fff18b27e730bfd473
travisbare <travisbare@gmail.com>
2019-06-01 11:50:07 -0500

    got rid of server side calls from front end

--- models/index.js
+++ models/index.js
@@ -2,5 +2,5 @@
 
 module.exports = {
   Article: require("./Article"),
-  Comments: require("./Comments")
+  Comments: require("./Comment")
 };
--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 
 // Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
+
 const axios = require("axios");
 const cheerio = require("cheerio");
 
 // Require all models
-//const db = require("./models");
-
+var models = require("./models");
+mongoose.Promise = Promise;
 // Initialize Express
 
 const app = express();
@@ -72,7 +71,7 @@ app.get("/scrape", function (req, res) {
                 .attr("href");
 
             // Create a new Article using the `result` object built from scraping
-            db.Article.create(result)
+            models.Article.create(result)
                 .then(function (dbArticle) {
                     // View the added result in the console
                     console.log(dbArticle);
@@ -91,7 +90,7 @@ app.get("/scrape", function (req, res) {
 // Route for getting all Articles from the db
 app.get("/articles", function (req, res) {
     // Grab every document in the Articles collection
-    db.Article.find({})
+    models.Article.find({})
         .then(function (dbArticle) {
             // If we were able to successfully find Articles, send them back to the client
             res.json(dbArticle);
@@ -105,7 +104,7 @@ app.get("/articles", function (req, res) {
 // Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function (req, res) {
     // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-    db.Article.findOne({ _id: req.params.id })
+    models.Article.findOne({ _id: req.params.id })
         // ..and populate all of the notes associated with it
         .populate("comment")
         .then(function (dbArticle) {
@@ -121,12 +120,12 @@ app.get("/articles/:id", function (req, res) {
 // Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function (req, res) {
     // Create a new note and pass the req.body to the entry
-    db.Comment.create(req.body)
+    models.Comment.create(req.body)
         .then(function (dbNote) {
             // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
             // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
             // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+            return models.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
         })
         .then(function (dbArticle) {
             // If we were able to successfully update an Article, send it back to the client
